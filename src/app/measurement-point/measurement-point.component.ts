d9d7e5f8a7ca3765525d0f3e1e67703c8ac93e82
thibaultcrn1 <tcarron6@outlook.com>
2021-05-21 00:14:43 +0200

    Update measurement point

--- src/app/measurement-point/measurement-point.component.ts
+++ src/app/measurement-point/measurement-point.component.ts
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeasurementPointComponent implements OnInit {
 
-  measurement = "Measurement Point";
+  title = 'Measurement Point';
+  charts = [];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
