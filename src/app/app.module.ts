0da68f1c22edf5c1f5eaaecdc458fc698a3290c2
thibaultcrn1 <tcarron6@outlook.com>
2021-06-01 02:50:00 +0200

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { ChartsModule } from 'ng2-charts';
  
 import { AppRoutingModule } from './app-routing.module';
@@ -17,7 +18,8 @@ import { MeasurementPointComponent } from './measurement-point/measurement-point
   imports: [
     BrowserModule,
     ChartsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
