1d10c3eb6c58a9a4ac6c1cdf5cdad3e8016956e6
thibaultcrn1 <tcarron6@outlook.com>
2021-06-06 03:40:48 +0200

    Change api adress

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
-  private REST_API_SERVER = "http://localhost:3000/products";
+  private REST_API_SERVER = "https://www.trackcorona.live/api/countries";
 
   constructor(private httpClient: HttpClient) { }
 
