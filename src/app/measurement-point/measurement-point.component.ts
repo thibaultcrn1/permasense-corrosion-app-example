54b766152a4570eb8f227a93b6e5e7bed2873dd4
thibaultcrn1 <tcarron6@outlook.com>
2021-06-09 00:32:46 +0200

    Update ts file

--- src/app/measurement-point/measurement-point.component.ts
+++ src/app/measurement-point/measurement-point.component.ts
@@ -23,30 +23,42 @@ export class MeasurementPointComponent implements OnInit {
     var myChart = new Chart("myChart", {
       type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Nord', 'Jura', 'Pas-De-Calais', 'Charente-Maritime'],
         datasets: [{
-          label: 'Test',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
+          label: 'Contaminés',
+          data: [12, 19, 3, 5],
+          backgroundColor: 'transparent',
+          borderColor: 'blue',
+          borderWidth: 2
+        },
+        {
+          label: 'Décés',
+          data: [3, 5, 2, 3],
+          backgroundColor: 'transparent',
+          borderColor: 'red',
+          borderWidth: 2
+        },
+        {
+          label: 'Réanimation',
+          data: [12, 19, 2, 3],
+          backgroundColor: 'transparent',
+          borderColor: 'green',
+          borderWidth: 2
+        },
+        {
+          label: 'Hospitalisations',
+          data: [12, 19, 3, 7],
+          backgroundColor: 'transparent',
+          borderColor: 'purple',
+          borderWidth: 2
         }]
       },
       options: {
+        elements: {
+          line: {
+            tension: 0
+          }
+        },
         scales: {
           yAxes: [{
             ticks: {
