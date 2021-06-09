eac6f78eb885c5bc98449ac619f42681ba3d7f8c
thibaultcrn1 <tcarron6@outlook.com>
2021-06-10 00:36:26 +0200

    Update

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { throwError } from 'rxjs';
 
+import { retry, catchError } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -26,6 +28,6 @@ export class DataService {
 
 
   public sendGetRequest() {
-    return this.httpClient.get(this.REST_API_SERVER);
+    return this.httpClient.get(this.REST_API_SERVER).pipe(catchError(this.handleError));
   }
 }
