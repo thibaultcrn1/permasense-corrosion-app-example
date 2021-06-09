2e9e88d581d919f0d21afd3d9c4def1378208eeb
thibaultcrn1 <tcarron6@outlook.com>
2021-06-10 00:37:58 +0200

    Update

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -28,6 +28,6 @@ export class DataService {
 
 
   public sendGetRequest() {
-    return this.httpClient.get(this.REST_API_SERVER).pipe(catchError(this.handleError));
+    return this.httpClient.get(this.REST_API_SERVER).pipe(retry(3), catchError(this.handleError));
   }
 }
