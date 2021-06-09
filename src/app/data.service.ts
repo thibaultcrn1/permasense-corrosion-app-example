1694e13db6afd85d08ee4fe993e332db7603490d
thibaultcrn1 <tcarron6@outlook.com>
2021-06-10 00:36:04 +0200

    Create HTTP Error handling

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,20 @@ export class DataService {
   private REST_API_SERVER = "https://coronavirusapi-france.vercel.app/AllLiveData";
 
   constructor(private httpClient: HttpClient) { }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = "Unknown error!";
+    if(error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `ERROR : ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error code : ${error.status}\nMessage : ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage)
+  }
+
 
   public sendGetRequest() {
     return this.httpClient.get(this.REST_API_SERVER);
